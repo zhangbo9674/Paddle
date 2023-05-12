607bbe8b4d60576cdb333047fad915f25068b4bf
zhangbo9674 <zhangbo54@baidu.com>
2023-05-12 10:06:49 +0000

    add code gen

--- paddle/fluid/dialect/op_gen.py
+++ paddle/fluid/dialect/op_gen.py
@@ -14,37 +14,15 @@
 
 import argparse
 import os
+import pathlib
+import sys
 
 import yaml
 
-# ===========================
-fake_op_name = "FakeOp"
-fake_op_h = """
-class FakeOp : public ir::Op<FakeOp> {
- public:
-  using Op::Op;
-  static const char *name() { return "Fake"; }
-  static const char **attributes_name_;
-  static uint32_t attributes_num() { return 0; }
-};
-"""
-fake_op_cc = """
-const char **FakeOp::attributes_name_ = nullptr;
-"""
-fake_op_name1 = "FakeOp1"
-fake_op_h1 = """
-class FakeOp1 : public ir::Op<FakeOp1> {
- public:
-  using Op::Op;
-  static const char *name() { return "Fake1"; }
-  static const char *attributes_name_[];
-  static uint32_t attributes_num() { return 1; }
-};
-"""
-fake_op_cc1 = """
-const char *FakeOp1::attributes_name_[] = {"parameter_name"};
-"""
-# ===========================
+sys.path.append(
+    str(pathlib.Path(__file__).parents[2].joinpath('phi/api/yaml/generator'))
+)
+from api_base import BaseAPI
 
 
 # Script parameter parsing
@@ -52,7 +30,7 @@ def ParseArguments():
     parser = argparse.ArgumentParser(
         description='Generate Paddle Dialect OP Definition Files'
     )
-    parser.add_argument('--api_yaml', type=str)
+    parser.add_argument('--op_yaml', type=str)
     parser.add_argument('--op_header_file', type=str)
     parser.add_argument('--op_source_file', type=str)
     parser.add_argument('--namespaces', type=str)
@@ -77,39 +55,187 @@ H_FILE_TEMPLATE = """
 
 CC_FILE_TEMPLATE = """
 #include "{h_file}"
+#include "paddle/phi/core/enforce.h"
+#include "paddle/ir/ir_context.h"
 
 {input}
 """
 
-OP_DECLARE_0_ATTRIBUTES_TEMPLATE = """
-class {op_name}Op : public ir::Op<{op_name}{interfaces}{traits}> {{
+OP_DECLARE_ATTRIBUTES_TEMPLATE = """
+class {op_name}Op : public ir::Op<{op_name}Op{interfaces}{traits}> {{
  public:
   using Op::Op;
-  static const char *name() {{ return "{op_name}"; }}
-  static const char **attributes_name_;
-  static uint32_t attributes_num() {{ return 0; }}
+  static const char *name() {{ return "{op_name}Op"; }}
+  {attribute_declare}
+  static constexpr uint32_t attributes_num = {attribute_num};
+  static void verify(const std::vector<ir::Type> &inputs, const std::vector<ir::Type> &outputs, ir::DictionaryAttribute attribute);
 }};
 """
-OP_DECLARE_N_ATTRIBUTES_TEMPLATE = """
-class {op_name}Op : public ir::Op<{op_name}{interfaces}{traits}> {{
- public:
-  using Op::Op;
-  static const char *name() {{ return "{op_name}"; }}
-  static const char *attributes_name_[];
-  static uint32_t attributes_num() {{ return {attribute_num}; }}
-}};
-"""
+op_0_attribute_declare_str = "static const char **attributes_name_;"
+op_n_attribute_declare_str = "static const char *attributes_name_[{}];"
 
-OP_DEFINED_0_ATTRIBUTES_TEMPLATE = """
+OP_0_ATTRIBUTE_DEFINED_TEMPLATE = """
 const char **{op_name}Op::attributes_name_ = nullptr;
 """
-OP_DEFINED_N_ATTRIBUTES_TEMPLATE = """
-const char *{op_name}Op::attributes_name_[] = {{ {attribute_names} }};
+OP_N_ATTRIBUTE_DEFINED_TEMPLATE = """
+const char *{op_name}Op::attributes_name_[{attribute_num}] = {{ {attribute_names} }};
 """
+
+OP_VERIFY_TEMPLATE = """
+void {op_name}Op::verify(const std::vector<ir::Type> &inputs, const std::vector<ir::Type> &outputs, ir::DictionaryAttribute attributes) {{
+  // verify inputs
+  PADDLE_ENFORCE_EQ(inputs.size(), {inputs_size},
+                    paddle::platform::errors::InvalidArgument("The size %d of inputs must be equal to {inputs_size}.", inputs.size()));
+  {inputs_type_check}
+  // verify outputs
+  PADDLE_ENFORCE_EQ(outputs.size(), {outputs_size},
+                    paddle::platform::errors::InvalidArgument("The size %d of inputs must be equal to {outputs_size}.", outputs.size()));
+  {outputs_type_check}
+  // verify attributes
+  {attributes_check}
+}}
+"""
+# Example: index=1, standard=paddle::dialect::DenseTensorType
+INPUT_TYPE_CHECK_TEMPLATE = """
+  PADDLE_ENFORCE_EQ(inputs[{index}].isa<{standard}>(), true, paddle::platform::errors::InvalidArgument("Type validation failed for the {index}th input."));
+"""
+INPUT_VECTORTYPE_CHECK_TEMPLATE = """
+  PADDLE_ENFORCE_EQ(inputs[{index}].isa<ir::VectorType>(), true, paddle::platform::errors::InvalidArgument("Type validation failed for the {index}th input."));
+  PADDLE_ENFORCE_EQ(inputs[{index}].InnerType().isa<{standard}>(), true, paddle::platform::errors::InvalidArgument("Type validation failed for the {index}th input."));
+"""
+OUTPUT_TYPE_CHECK_TEMPLATE = """
+  PADDLE_ENFORCE_EQ(outputs[{index}].isa<{standard}>(), true, paddle::platform::errors::InvalidArgument("Type validation failed for the {index}th output."));
+"""
+OUTPUT_VECTORTYPE_CHECK_TEMPLATE = """
+  PADDLE_ENFORCE_EQ(outputs[{index}].isa<ir::VectorType>(), true, paddle::platform::errors::InvalidArgument("Type validation failed for the {index}th output."));
+  PADDLE_ENFORCE_EQ(outputs[{index}].InnerType().isa<{standard}>(), true, paddle::platform::errors::InvalidArgument("Type validation failed for the {index}th output."));
+"""
+# Example: attribute_name=xxx
+ATTRIBUTE_CHECK_TEMPLATE = """
+  PADDLE_ENFORCE_EQ(attributes.GetValue(ir::StrAttribute::get(ir::IrContext::Instance(), std::string("{attribute_name}"))), true,
+                    paddle::platform::errors::InvalidArgument("Missing necessary attribute: {attribute_name}."));
+  PADDLE_ENFORCE_EQ(attributes.GetValue(ir::StrAttribute::get(ir::IrContext::Instance(), std::string("{attribute_name}"))).isa<{standard}>(), true,
+                    paddle::platform::errors::InvalidArgument("Type of attribute: {attribute_name} is not right."));
+"""
+ATTRIBUTE_VECTOR_CHECK_TEMPLATE = """
+  PADDLE_ENFORCE_EQ(attributes.GetValue(ir::StrAttribute::get(ir::IrContext::Instance(), std::string("{attribute_name}"))), true,
+                    paddle::platform::errors::InvalidArgument("Missing necessary attribute: {attribute_name}."));
+  PADDLE_ENFORCE_EQ(attributes.GetValue(ir::StrAttribute::get(ir::IrContext::Instance(), std::string("{attribute_name}"))).isa<ir::VectorAttribute>(), true,
+                    paddle::platform::errors::InvalidArgument("Type of attribute: {attribute_name} is not right."));
+  PADDLE_ENFORCE_EQ(attributes.GetValue(ir::StrAttribute::get(ir::IrContext::Instance(), std::string("{attribute_name}"))).InnerAttribute().isa<{standard}>(), true,
+                    paddle::platform::errors::InvalidArgument("Type of attribute: {attribute_name} is not right."));
+"""
+
+
+class OpInfo(BaseAPI):
+    def __init__(self, op_item_yaml):
+        # api: string, api name
+        # inputs:
+        #     names : [], list of input names
+        #     input_info : {input_name : type}
+        # attrs:
+        #     names : [], list of attribute names
+        #     attr_info : { attr_name : (type, default_values)}
+        # outputs:
+        #     names : [], list of output names
+        #     types : [], list of output types
+        #     out_size_expr : [], expression for getting size of vector<Tensor>
+        super().__init__(op_item_yaml)
+
+        # input/output Paddle Type -> ir Type
+        self.type_map = {
+            'Tensor': 'paddle::dialect::DenseTensor',
+            'std::vector<Tensor>': 'ir:VectorType<paddle::dialect::DenseTensor>',
+            'const Tensor&': 'paddle::dialect::DenseTensor',
+            'const std::vector<Tensor>&': 'ir:VectorType<paddle::dialect::DenseTensor>',
+            'const paddle::optional<Tensor>&': 'paddle::dialect::DenseTensor',
+            'const paddle::optional<std::vector<Tensor>>&': 'ir:VectorType<paddle::dialect::DenseTensor>',
+            'paddle::optional<int>': 'ir::IntType',
+            'paddle::optional<int32_t>': 'ir::Int32Type',
+            'paddle::optional<int64_t>': 'ir::Int64Type',
+            'paddle::optional<float>': 'ir::FloatType',
+            'paddle::optional<double>': 'ir::DoubleType',
+            'paddle::optional<bool>': 'ir::BoolType',
+            'paddle::optional<const Place&>': 'ir::PlaceType',
+            'paddle::optional<DataLayout>': 'ir::DataLayoutType',
+            'paddle::optional<DataType>': 'ir::DataTypeType',
+        }
+        # attribute Paddle Type -> ir Attribute
+        self.attr_type_map = {
+            'const IntArray&': 'paddle::dialect::IntArray',
+            'const Scalar&': 'paddle::dialect::Scalar',
+            'const std::vector<phi::Scalar>&': 'ir::VectorAttribute<paddle::dialect::Scalar>',
+            'int': 'ir::IntAttribute',
+            'int32_t': 'ir::Int32Attribute',
+            'int64_t': 'ir::Int64Attribute',
+            'long': 'ir::LongAttribute',
+            'size_t': 'ir::Size_tAttribute',
+            'float': 'ir::FloatAttribute',
+            'const std::vector<float>&': 'ir::VectorAttribute<ir::FloatAttribute>',
+            'double': 'ir::DoubleAttribute',
+            'bool': 'ir::BoolAttribute',
+            'const std::vector<bool>&': 'ir::VectorAttribute<ir::BoolAttribute>',
+            'const std::string&': 'ir::StrAttribute',
+            'const std::vector<std::string>&': 'ir::VectorAttribute<ir::StrAttribute>',
+            'const Place&': 'paddle::dialect::PlaceAttribute',
+            'DataLayout': 'paddle::dialect::DataLayoutAttribute',
+            'DataType': 'paddle::dialect::DataType::Attribute',
+            'const std::vector<int64_t>&': 'ir::VectorAttribute<ir::Int64Attribute>',
+            'const std::vector<int>&': 'ir::VectorAttribute<ir::IntAttribute>',
+        }
+
+    def get_op_name(self):
+        return self.api
+
+    def get_inputs_name_list(self):
+        return self.inputs['names']
+
+    def get_inputs_type_list(self, input_type_map={}):
+        if len(input_type_map) == 0:
+            input_type_map = self.type_map
+        inputs_type_list = []
+        for name in self.get_inputs_name_list():
+            inputs_type_list.append(
+                input_type_map[self.inputs['input_info'][name]]
+            )
+        return inputs_type_list
+
+    def get_outputs_type_list(self, output_type_map={}):
+        if len(output_type_map) == 0:
+            output_type_map = self.type_map
+        outputs_type_list = []
+        for type in self.outputs['types']:
+            outputs_type_list.append(output_type_map[type])
+        return outputs_type_list
+
+    def get_attributes_name_list(self):
+        return self.attrs['names']
+
+    def get_attributes_type_list(self, attribute_type_map={}):
+        if len(attribute_type_map) == 0:
+            attribute_type_map = self.attr_type_map
+        attributes_type_list = []
+        for name in self.get_attributes_name_list():
+            attributes_type_list.append(
+                attribute_type_map[self.attrs['attr_info'][name][0]]
+            )
+        return attributes_type_list
+
+    def get_interfaces_list(self):
+        """
+        To be continue...
+        """
+        return []
+
+    def get_traits_list(self):
+        """
+        To be continue...
+        """
+        return []
 
 
 # Generate files
-def GenerateOpDefFile(api_yaml_files, header_file, source_file, namespaces):
+def GenerateOpDefFile(op_yaml_files, header_file, source_file, namespaces):
     # (1) Delete existing old files: pd_op.h.tmp, pd_op.cc.tmp
     if os.path.exists(header_file):
         os.remove(header_file)
@@ -117,29 +243,129 @@ def GenerateOpDefFile(api_yaml_files, header_file, source_file, namespaces):
         os.remove(source_file)
 
     # (2) Traverse the content of yaml files to generate op definitions
-    op_name_list = []  # all op class name store in this list
-    op_declare_list = []  # all op class declare store in this list
-    op_defined_list = []  # all op class defined store in this list
-    op_name_list.append(fake_op_name)
-    op_name_list.append(fake_op_name1)
-    op_declare_list.append(fake_op_h)
-    op_declare_list.append(fake_op_h1)
-    op_defined_list.append(fake_op_cc)
-    op_defined_list.append(fake_op_cc1)
-
     ops_yaml = []
-    for each_api_yaml in api_yaml_files:
-        with open(each_api_yaml, 'r') as f:
+    for each_op_yaml in op_yaml_files:
+        with open(each_op_yaml, 'r') as f:
             api_list = yaml.load(f, Loader=yaml.FullLoader)
             if api_list:
                 ops_yaml.extend(api_list)
-    print("ops_yaml: \n", ops_yaml)
+    ops_info = []
+    for op_yaml in ops_yaml:
+        ops_info.append(OpInfo(op_yaml))
+
+    ops_name_list = []  # all op class name store in this list
+    ops_declare_list = []  # all op class declare store in this list
+    ops_defined_list = []  # all op class defined store in this list
+
+    for op in ops_info:
+        # get op info
+        op_name = op.get_op_name()
+        op_inputs_type = op.get_inputs_type_list()
+        op_outputs_type = op.get_outputs_type_list()
+        op_attributes_name = op.get_attributes_name_list()
+        op_attributes_type = op.get_attributes_type_list()
+        op_interfaces = op.get_interfaces_list()
+        op_traits = op.get_traits_list()
+
+        # auto code generate
+        if len(op_interfaces) > 0:
+            op_interfaces_str = "," + ",".join(op_interfaces)
+        else:
+            op_interfaces_str = ""
+        if len(op_interfaces) > 0:
+            op_traits_str = "," + ",".join(op_traits)
+        else:
+            op_traits_str = ""
+
+        if len(op_attributes_name) == 0:
+            op_declare_str = OP_DECLARE_ATTRIBUTES_TEMPLATE.format(
+                op_name=op_name,
+                interfaces=op_interfaces_str,
+                traits=op_traits_str,
+                attribute_declare=op_0_attribute_declare_str,
+                attribute_num=0,
+            )
+            op_defined_str = OP_0_ATTRIBUTE_DEFINED_TEMPLATE.format(
+                op_name=op_name
+            )
+        else:
+            op_declare_str = OP_DECLARE_ATTRIBUTES_TEMPLATE.format(
+                op_name=op_name,
+                interfaces=op_interfaces_str,
+                traits=op_traits_str,
+                attribute_declare=op_n_attribute_declare_str.format(
+                    len(op_attributes_name)
+                ),
+                attribute_num=len(op_attributes_name),
+            )
+            attribute_names_str = '"' + '", "'.join(op_attributes_name) + '"'
+            op_defined_str = OP_N_ATTRIBUTE_DEFINED_TEMPLATE.format(
+                op_name=op_name,
+                attribute_num=len(op_attributes_name),
+                attribute_names=attribute_names_str,
+            )
+
+        inputs_type_check_str = ""
+        for idx in range(len(op_inputs_type)):
+            input_type = op_inputs_type[idx]
+            if input_type.startswith("ir:VectorType"):
+                inner_type = input_type[14:-1]
+                inputs_type_check_str += INPUT_VECTORTYPE_CHECK_TEMPLATE.format(
+                    index=idx, standard=inner_type
+                )
+            else:
+                inputs_type_check_str += INPUT_TYPE_CHECK_TEMPLATE.format(
+                    index=idx, standard=input_type
+                )
+
+        outputs_type_check_str = ""
+        for idx in range(len(op_outputs_type)):
+            output_type = op_outputs_type[idx]
+            if output_type.startswith("ir:VectorType"):
+                inner_type = output_type[14:-1]
+                outputs_type_check_str += (
+                    OUTPUT_VECTORTYPE_CHECK_TEMPLATE.format(
+                        index=idx, standard=inner_type
+                    )
+                )
+            else:
+                outputs_type_check_str += OUTPUT_TYPE_CHECK_TEMPLATE.format(
+                    index=idx, standard=output_type
+                )
+
+        attributes_check_str = ""
+        for idx in range(len(op_attributes_name)):
+            attribute_name = op_attributes_name[idx]
+            attribute_type = op_attributes_type[idx]
+            if attribute_type.startswith("ir:VectorAttribute"):
+                inner_attribute = attribute_type[18:-1]
+                attributes_check_str += ATTRIBUTE_VECTOR_CHECK_TEMPLATE.format(
+                    attribute_name=attribute_name, standard=attribute_type
+                )
+            else:
+                attributes_check_str += ATTRIBUTE_CHECK_TEMPLATE.format(
+                    attribute_name=attribute_name, standard=attribute_type
+                )
+
+        op_verify_str = OP_VERIFY_TEMPLATE.format(
+            op_name=op_name,
+            inputs_size=len(op_inputs_type),
+            outputs_size=len(op_outputs_type),
+            inputs_type_check=inputs_type_check_str,
+            outputs_type_check=outputs_type_check_str,
+            attributes_check=attributes_check_str,
+        )
+
+        ops_name_list.append(op_name)
+        ops_declare_list.append(op_declare_str)
+        ops_defined_list.append(op_defined_str)
+        ops_defined_list.append(op_verify_str)
 
     # (3) Generate head file str
     head_file_str = """\n#define GET_PD_DIALECT_OP_LIST {}\n""".format(
-        ", ".join(op_name_list)
+        ", ".join(ops_name_list)
     )  # Add GET_PD_DIALECT_OP_LIST
-    head_file_str += "".join(op_declare_list)  # Add op class
+    head_file_str += "".join(ops_declare_list)  # Add op class
     for name in reversed(namespaces):
         head_file_str = NAMESPACE_GARD_TEMPLATE.format(
             namespace=name, input=head_file_str
@@ -147,7 +373,7 @@ def GenerateOpDefFile(api_yaml_files, header_file, source_file, namespaces):
     head_file_str = H_FILE_TEMPLATE.format(input=head_file_str)  # Add head
 
     # (4) Generate source file str
-    source_file_str = "".join(op_defined_list)  # Add op define
+    source_file_str = "".join(ops_defined_list)  # Add op define
     for name in reversed(namespaces):
         source_file_str = NAMESPACE_GARD_TEMPLATE.format(
             namespace=name, input=source_file_str
@@ -167,7 +393,7 @@ def GenerateOpDefFile(api_yaml_files, header_file, source_file, namespaces):
 if __name__ == "__main__":
     args = ParseArguments()
 
-    api_yaml_files = args.api_yaml.split(",")
+    op_yaml_files = args.op_yaml.split(",")
     header_file = args.op_header_file
     source_file = args.op_source_file
     if args.namespaces is not None:
@@ -175,4 +401,4 @@ if __name__ == "__main__":
     else:
         namespaces = []
 
-    GenerateOpDefFile(api_yaml_files, header_file, source_file, namespaces)
+    GenerateOpDefFile(op_yaml_files, header_file, source_file, namespaces)
